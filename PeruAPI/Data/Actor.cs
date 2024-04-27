c98dd375996b10d3671be338e501f6275db4f04c
Luis Chang <luis.chang@outlook.com>
2024-04-27 17:47:54 -0500

    Agregar archivos de proyecto.

---
+++ PeruAPI/Data/Actor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeruAPI.Data;
+
+public partial class Actor
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+}
